8914f291056736a908d714cc2353973f7935c1bd
edith.githinji <edithgithinji2020@gmail.com>
2024-12-05 11:12:17 +0300

    the objects assignment.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+
+
+
+const employee = {
+    name: "Edith",
+    streetAddress: "Nairobi",
+  };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+ function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam",
+    employee.streetAddress = "12 Broadway";
+        
+       return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
+ 
